eaac0f502dcfd965abbfcf30895bdb403105284d
nhuse <nathan.huse@yahoo.com>
2021-08-31 17:26:02 -0500

    commit tree 6a12a4f2d5fa94795f2554ec45e150f05e05557f

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validate :title_is_clickbait
+
+    def title_is_clickbait
+        acceptable_strings = ["Won't Believe", "Secret", "Top ", "Guess"]
+        unless title.include?("Won't Believe")
+            errors.add(:category, "Title must be clickbaity")
+        end
+    end
+
 end
